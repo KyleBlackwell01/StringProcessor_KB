f00cbebb083dfa6c6744baae66ce81a8f340a193
KYLE BLACKWELL <102472889@student.swin.edu.au>
2019-08-21 09:18:50 +1000

    Completed StringProcessor

---
+++ StringProcessor/StringProcessor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace StringProcessor
+{
+    public class Processor
+    {
+        /// <summary>
+        /// Returns number of char found in word.  Case-insensitive.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public int CountChar(string word, char c) {
+            return word.Count(x => char.ToUpperInvariant(x) == char.ToUpperInvariant(c));
+
+            //throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns word in reverse.  Case sensitive.
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public string Reverse(string word) {
+            char[] wArray = word.ToCharArray();
+            Array.Reverse(wArray);
+            return new string(wArray);
+
+            //throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Replaces all spaces in a word with char.  Case sensitive.  Replaces pre and trailing spaces
+        /// </summary>
+        /// <param name="word"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public string ReplaceSpace(string word, char c) {
+
+            return word.Replace(" ", c.ToString());
+            //throw new NotImplementedException();
+        }
+    }
+}
